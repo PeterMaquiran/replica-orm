899e0f286f2892aae2c5368e6c72a5d98680b2ec
Peter Maquiran <maquiranpeter29@gmail.com>
2026-06-04 20:58:59 +0100

    chore(commitlint): tighten rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -8,7 +8,13 @@ export default {
     ],
     'type-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
-    ///
+    ////
     'type-empty': [2, 'never'],
+    // ✅ LIMIT SCOPE LENGTH
+    'scope-max-length': [2, 'always', 10],
+    'type-max-length': [2, 'always', 10],
+    'subject-max-length': [2, 'always', 100],
+    // ❌ disallow footer
+    'footer-max-length': [2, 'always', 0],
   },
 }
