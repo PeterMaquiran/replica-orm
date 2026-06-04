38e958f7b04b24f123c9be0ab213942d48133b62
Peter Maquiran <maquiranpeter29@gmail.com>
2026-06-04 20:46:09 +0100

    test: add commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,18 +1,14 @@
 export default {
   extends: ['@commitlint/config-conventional'],
-
   rules: {
     'type-enum': [
       2,
       'always',
       ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test', 'perf'],
     ],
-
     'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
-
-    // 👇 ADD IT HERE.
-    'header-pattern': [2, 'always', '^[a-z]+: .+$'],
+    //
+    'type-empty': [2, 'never'],
   },
 }
